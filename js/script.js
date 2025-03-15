35b5173584b03e98b105641a017fe9284bb34b00
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-03-15 21:05:44 +0300

    created the sketch outline figma image

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const buildings = [
+  {
+    id: 1,
+    name: "Futuristic Villa",
+    architect: "Zaha Hadid Architects",
+    image: "villa.jpg",
+    rating: 5,
+    comment: "Amazing design!",
+  },
+  {
+    id: 2,
+    name: "Eco Skyscraper",
+    architect: "Norman Foster",
+    image: "eco-skyscraper.jpg",
+    rating: 4,
+    comment: "Sustainable and modern!",
+  },
+  {
+    id: 3,
+    name: "Minimalist House",
+    architect: "Tadao Ando",
+    image: "minimalist-house.jpg",
+  },
+];
